99d71759b196934cd0b1fa70fbe216cdc69a6652
mahbubul14 <mahbub21alam2017@gmailcom>
2021-05-21 04:23:27 +0600

    Solve drawing book in simple way

--- practice.rb
+++ practice.rb
@@ -1,31 +1,16 @@
 def pageCount(n, p)
-  # ........from page 1......
-  f_page_rem = p%2
-
-  if f_page_rem == 0
+  # Write your code here
+  if n.even?
     f_page = p/2
-
+    l_page = (n-p+1)/2
   else
-    f_page = (p-1)/2
+    f_page = p/2
+    l_page = (n-p)/2
   end
-  # ........from last page..........
-  if n.even? || n.odd? && n != 2
-   l_page_result = (n-p)/2
-  elsif n.odd? && p.even? && p > 1
-   l_page_result = (n - (p+1))/2
-  elsif p == n
-   l_page_result = 0
+  if f_page < l_page
+    p f_page
+  else 
+    p l_page
   end
-# ........compare results.......
-  if f_page < l_page_result
-    result = f_page
-  elsif n == 2 && p == 1
-    result =0
-  elsif n.even? && n-p == 1
-    result =1
-  else
-    result = l_page_result
-  end
-    p result
 end
-pageCount(2, 1)
+pageCount(5, 4)
--- test.rb
+++ test.rb
@@ -1,14 +1,5 @@
 def pageCount(n, p)
-    # Write your code here
-    m_page = (n/2.to_f).floor
 
-    if n.even?
-      p f_page_result = m_page/2
-       l_page = n - m_page
-       l_page_result = l_page/2
-    elsif n.odd?
-        p f_page_result = p - 1
-        p l_page_result = n - p
-    end
+    puts n.even? true || false
 end
-pageCount(5,4)
+pageCount(6, 4)
