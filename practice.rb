867f6645655f024e0c427643eb75a71bd052a19a
mahbubul14 <mahbub21alam2017@gmailcom>
2021-05-21 01:27:55 +0600

    Solve drawing book challenge

--- practice.rb
+++ practice.rb
@@ -1,27 +1,31 @@
 def pageCount(n, p)
-  # Write your code here
-  l_page = n-p
-  l_page_rem = l_page % 2
-
-  if l_page_rem == 0
-    l_page_result = l_page/2
-  else
-    l_page_result = (l_page-1)/2
-  end
-
+  # ........from page 1......
   f_page_rem = p%2
 
   if f_page_rem == 0
     f_page = p/2
+
   else
     f_page = (p-1)/2
   end
-
+  # ........from last page..........
+  if n.even? || n.odd? && n != 2
+   l_page_result = (n-p)/2
+  elsif n.odd? && p.even? && p > 1
+   l_page_result = (n - (p+1))/2
+  elsif p == n
+   l_page_result = 0
+  end
+# ........compare results.......
   if f_page < l_page_result
     result = f_page
+  elsif n == 2 && p == 1
+    result =0
+  elsif n.even? && n-p == 1
+    result =1
   else
     result = l_page_result
   end
-  puts result
+    p result
 end
-pageCount(5, 3)
+pageCount(2, 1)
--- test.rb
+++ test.rb
@@ -1,7 +1,27 @@
-def repeatedString(s, n)
-    splited = s.chars()
-    splited.each do |i|
-        puts i += splited
+def pageCount(n, p)
+    # Write your code here
+    l_page = n-p
+    p l_page_rem = l_page % 2
+  
+    if l_page_rem == 0
+      p l_page_result = l_page/2
+    else
+     p l_page_result = (l_page-1)/2
     end
-end
-repeatedString("aba", 10)
+  
+    f_page_rem = p%2
+  
+    if f_page_rem == 0
+      f_page = p/2
+    else
+      f_page = (p-1)/2
+    end
+  
+    if f_page < l_page_result
+      result = f_page
+    else
+      result = l_page_result
+    end
+    result
+  end
+  pageCount(5, 2)
