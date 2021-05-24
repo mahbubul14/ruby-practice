cce47675567b7cf69de458260ab9b90f4111369d
mahbubul14 <mahbub21alam2017@gmailcom>
2021-05-24 20:54:08 +0600

    Clear test file

--- practice.rb
+++ practice.rb
@@ -1,11 +1,11 @@
 def pickingNumbers(a)
-  # Write your code here
+
   arr = a.sort
-  # arr << a.sort
-  arr.each do|i|
-    p arr[0] -= arr[i+1]
+  count = 0
+  for i in arr do
+    count =  i- i[i+1]
   end
+  puts count
 end
 pickingNumbers([4, 6, 5, 3, 3, 1])
-
 # [1, 3, 3, 4, 5, 6]
