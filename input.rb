f3e384d460431a315156685cf7d532a847385721
mahbubul14 <mahbub21alam2017@gmailcom>
2021-06-18 22:53:27 +0600

    Complete hurdle race coding challenges

--- input.rb
+++ input.rb
@@ -1,4 +1,11 @@
-pickingNumbers([1,3,3,4,5,6])
-
-input = [1,1,2,2,4,4,5,5,5]
-p pickingNumbers(input)
+def hurdleRace(k, height)
+    # Write your code here
+  p h = height.max.to_i
+  p j = k[1].to_i
+if j<h 
+    p h-j
+else
+    p 0
+end
+  end
+  hurdleRace([5,7],[1, 6, 3, 5, 2])
--- practice.rb
+++ practice.rb
@@ -1,7 +1,13 @@
 def hurdleRace(k, height)
   # Write your code here
+j = k[1].to_i
+h = height.max.to_i
 
-p height.max.to_i - k.join.to_i
+if j < h
+  p h - j
+else
+  p 0
+end
 end
 
-hurdleRace([4], [1, 6, 3, 5, 2])
+hurdleRace([5, 4], [1, 6, 3, 5, 2])
