b9115ede1d81bab48eae51ebba963c0b0756bd59
mahbubul14 <mahbub21alam2017@gmailcom>
2021-07-08 19:51:08 +0600

    Create object and function

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-let firstNumber = 12, secondNumber = 4;
-console.log(firstNumber, secondNumber);
+// let car = {
+//     brand: 'audi',
+//     model: 2021,
+//     price: 4500
+// };
+
+// let cars = [
+//     {brand: 'tesla', model: 2021},
+//     {brand: 'tata', modeel: 2020}
+// ]
+// console.log(cars[0].model)
+let carName = 'corolla'
+function displayCar(){
+    console.log('this is', carName)
+}
+displayCar()
