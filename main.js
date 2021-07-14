52d4bbdb2e2a75cf0a9799116c359389fd9c700e
mahbubul14 <mahbub21alam2017@gmailcom>
2021-07-14 16:26:32 +0600

    Update finddigit function

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
-// let car = {
-//     brand: 'audi',
-//     model: 2021,
-//     price: 4500
-// };
-
-// let cars = [
-//     {brand: 'tesla', model: 2021},
-//     {brand: 'tata', modeel: 2020}
-// ]
-// console.log(cars[0].model)
-let carName = 'corolla'
-function displayCar(){
-    console.log('this is', carName)
+function findDigits(n) {
+    // Write your code here
+console.log(n.split())
 }
-displayCar()
+findDigits(12)
