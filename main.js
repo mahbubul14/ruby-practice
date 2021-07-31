3b9689368600a089ba6c12caa9b0f42a3206f943
mahbubul14 <mahbub21alam2017@gmailcom>
2021-07-31 22:06:48 +0600

    Add file for testing

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 function pageCount(n, p) {
     // Write your code here
-console.log(p);
+    halfBook = n/2;
+    leftSide = p/2;
+    rightSide = (halfBook - leftSide);
+    result = Math.min(halfBook - rightSide)
+    console.log(result);
 }
-pageCount(5, 4);
+pageCount(11, 4);
+
+// public class DrawingBook {
+
+// 	static int pageCount(int n, int p) {
+
+// 		int totalPageTurnCountFromFront = n / 2;
+// 		int targetPageTurnCountFromFront = p / 2;
+// 		int targetPageTurnCountFromBack = totalPageTurnCountFromFront - targetPageTurnCountFromFront;
+
+// 		return Math.min(targetPageTurnCountFromFront, targetPageTurnCountFromBack);
+
+// 	}
