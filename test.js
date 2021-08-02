53c89f4c6b32a343fdd14127abcae0c083fff2d2
mahbubul14 <mahbub21alam2017@gmailcom>
2021-08-02 09:37:01 +0600

    Update loop

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+console.log(condiments.length)
+// const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
 
-const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
-
-condiments[0] = 'Mayo';
-console.log(condiments);
-condiments = ['Mayo'];
-console.log(condiments);
+// condiments[0] = 'Mayo';
+// console.log(condiments);
+// condiments = ['Mayo'];
+// console.log(condiments);
