5318d634b0903f90f9a63282899b88661acff038
mahbubul14 <mahbub21alam2017@gmailcom>
2021-08-09 11:41:32 +0600

    Test obj in js

---
+++ dom.js
@@ -0,0 +1,18 @@
+let employee = {
+firstName: 'peter',
+'last name': "doe",
+employeeId: 2
+}
+// console.log(employee.firstName);
+// console.log(employee['last name']);
+// console.log(employee['employeeId']);
+// console.log('age' in employee);
+// console.log('firstName' in employee);
+// console.log(employee.sex = "male")
+// for(const key in employee){
+//     console.log(key)
+// };
+employee.greet = function (){
+    console.log('Hello');
+}
+employee.greet();
