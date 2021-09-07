5b75053f12108af1d5717b44b69412b55c07a281
mahbub <mahbub21alam2017@gmail.com>
2021-09-07 23:36:20 +0600

    Add basic html

--- practice/main.js
+++ practice/main.js
@@ -1,2 +1,13 @@
-const p = document.getElementById('message');
-console.log(p);
+let nMenu = document.createElement('li');
+nMenu.textContent = 'hello';
+menu.appendChild(nMenu);
+
+const email= document.querySelector('#mail');
+
+email.addEventListener("input", function(event){
+    if (email.validity.typeMismatch){
+        email.setCustomValidity("Input valid email address");
+    } else {
+        email.setCustomValidity("");
+    }
+});
