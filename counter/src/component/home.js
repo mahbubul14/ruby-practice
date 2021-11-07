495772690bde493f6b665f2d90e53b4559629bd3
mahbub <mahbub21alam2017@gmail.com>
2021-11-07 20:08:07 +0600

    Create rules for counter

--- counter/src/component/home.js
+++ counter/src/component/home.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Home = () =>
-<div>
-  <h1>Welcome to Counter Web App</h1>
-  <h2>Count:{}</h2>
-  <button type="submit">Increment</button>
-  <br/>
-  <button type="submit">Decrement</button>
-</div>
+const Home = () => {
+  const [count, setState] = useState(0)
+
+  const incrementHandler = () => {
+    setState(count+1)
+  }
+
+  const decrementHandler = () => {
+    setState(count-1)
+  }
+
+  const resettHandler = () => {
+    setState(0)
+  }
+
+  return (
+  <div>
+    <h1>Welcome to Counter Web App</h1>
+    <h2>Count:{count}</h2>
+    <button type="submit" onClick={incrementHandler}>Increment</button>
+    <br/>
+    <button type="submit" onClick={decrementHandler}>Decrement</button>
+    <button type="submit" onClick={resettHandler}>Reset</button>
+  </div>
+  )
+}
 
 export default Home;
