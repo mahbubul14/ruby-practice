20c9821f7d9ddf76ce1ea9b274d0f91ef543da06
Mahbub <mahbub21alam2017@gmail.com>
2021-11-30 15:15:53 +0600

    Add miad libs game

--- calculator.rb
+++
@@ -1,8 +0,0 @@
-puts 'Enter a number:'
-num1 = gets.chomp()
-
-puts 'Enter a number:'
-num2 = gets.chomp()
-
-
-puts (num1.to_f + num2.to_f)
---
+++ practice.rb
@@ -0,0 +1,20 @@
+# puts 'Enter a number:'
+# num1 = gets.chomp()
+
+# puts 'Enter a number:'
+# num2 = gets.chomp()
+
+# puts (num1.to_f + num2.to_f)
+
+puts "Enter a color: "
+color = gets.chomp()
+
+puts "Enter a plural_noun: "
+plural_noun = gets.chomp()
+
+puts "Enter a celebrity: "
+celebrity = gets.chomp()
+
+puts ("Roses are " + color)
+puts (plural_noun + " are blue")
+puts ("I love " + celebrity)
