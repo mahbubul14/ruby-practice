3adede48f364aa12fac4d2da009c14e072c4cbbb
Mahbub <mahbub21alam2017@gmail.com>
2021-11-30 15:05:43 +0600

    Add a small calculator for addition only

---
+++ calculator.rb
@@ -0,0 +1,8 @@
+puts 'Enter a number:'
+num1 = gets.chomp()
+
+puts 'Enter a number:'
+num2 = gets.chomp()
+
+
+puts (num1.to_f + num2.to_f)
--- test.rb
+++ test.rb
@@ -1,2 +1,9 @@
-print "Hello world"
-print "hello world"
+puts "Hello world"
+
+puts "Enter your name"
+name = gets.chomp()
+
+puts "Enter your age"
+age = gets.chomp()
+
+puts (" Hello " + name + ", You are cool! " + age)
