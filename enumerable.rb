5e14ceac850b747552f21607b4818c25c0550a0d
Mahbub <mahbub21alam2017@gmail.com>
2021-12-03 15:30:50 +0600

    Add a select enumerable method

---
+++ enumerable.rb
@@ -0,0 +1,13 @@
+# friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+
+# invited_list = []
+
+# for friend in friends do
+#   if friend != 'Brian'
+#   invited_list.push(friend)
+#   end
+# end
+# puts invited_list
+
+friends = ['Sharon', 'Leo', 'Leila', 'Brian', 'Arun']
+friends.select { |friend| friend != 'Brian'}
