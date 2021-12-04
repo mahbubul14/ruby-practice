9373b30623bd18b349ec1aaab07c942a3df818ef
Mahbub <mahbub21alam2017@gmail.com>
2021-12-04 23:20:26 +0600

    Add concatenation operator instead of push

--- test.rb
+++ test.rb
@@ -11,3 +11,7 @@ puts (" Hello " + name + ", You are cool! " + age)
 [1, 2, 3].each { |x| puts x * 10 }
 
 "L".upto("P") { |letter| puts letter }
+
+# instead of push we use << operator here
+alphabet = ["a", "b", "c"] << "d"
+caption = "A giraffe surrounded by " << "weezards!"
