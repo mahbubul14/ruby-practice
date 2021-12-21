9705f00cf31e05681d7c2710b3ca13e97cbfe410
mahbubul14 <mahbub21alam2017@gmail.com>
2021-12-21 16:19:29 +0600

    Create constructor and instance vars

--- claass.rb
+++ claass.rb
@@ -1,8 +1,12 @@
-class Person
-  def initialize(name)
-    @name = name
+class Animal
+  def initialize
+    @id = Random.rand(1..1000)
+    @name = "Rex"
+    @number_of_legs = 4
   end
 end
-person = Person.new("Maha")
+animal_1 = Animal.new
+animal_2 = Animal.new
 
-p person
+p animal_1
+p animal_2
