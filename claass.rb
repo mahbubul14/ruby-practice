83ff2b8b4fea9cad19e55db0e4e53cb32173e062
mahbubul14 <mahbub21alam2017@gmail.com>
2021-12-21 16:23:32 +0600

    Constructor with optional parameters

--- claass.rb
+++ claass.rb
@@ -1,12 +1,14 @@
 class Animal
-  def initialize(name, number_of_legs)
+  def initialize(name = "unknown", number_of_legs)
     @id = Random.rand(1..1000)
     @name = name
     @number_of_legs = number_of_legs
   end
 end
+# animal_1 = Animal.new("Rex", 4)
+# animal_2 = Animal.new("Bob", 8)
 animal_1 = Animal.new("Rex", 4)
-animal_2 = Animal.new("Bob", 8)
+animal_2 = Animal.new(8)
 
 p animal_1
 p animal_2
