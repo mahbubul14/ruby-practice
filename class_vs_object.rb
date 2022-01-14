309ec5732908caee491c6ea2ce49990cb60e37c8
mahbubul14 <mahbub21alam2017@gmail.com>
2022-01-14 18:29:15 +0600

    Update files

--- class_vs_object.rb
+++ class_vs_object.rb
@@ -1,10 +1,19 @@
+# rubocop:disable Style/Documentation
+# frozen_string_literal: true
+
 class Animal
   def initialize(name, age)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
   end
+
+  def speak
+    'bla bla bla'
+  end
 end
 
-animal_1 = Animal.new('dog', 12)
-print animal_1
+# rubocop:enable Style/Documentation
+
+p animal1 = Animal.new('dog', 12)
+print animal1.speak
